e33a93d1f8515b374dd9c76be8619c38f446787f
Guillermo Caracuel <hiltzaile@gmail.com>
2011-03-17 12:20:32 +0100

    initialize |master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libman::Application.config.secret_token = '7bde80affe1bd13592d3ada01b57217d913089ed17ce1dd9d4c763218ff5c714620a33d80020f278b038fc7826c48605b53fb51ddebf6b88150697e6e530bcdb'
